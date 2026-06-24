1340ddf2f828ed139229ce6b8fd14bc225952933
Matheus Leffa <matheusleffa@live.com>
2026-06-24 09:22:41 -0300

    desafio array de frutas

---
+++ moduloJS/desafios/arraysFrutas/scriptFrutas.js
@@ -0,0 +1,10 @@
+let frutas = ["Bergamota", "Maçã", "Uva", "Tomate", "Melancia"];
+console.log(frutas);
+console.log(frutas[2]);
+
+frutas[1] = "Laranja";
+console.log(frutas);
+
+frutas.push("Limão");
+frutas.push("Jaca");
+console.log(frutas);
